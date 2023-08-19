55bfb3d90ea149136637d5f42cbfc1f00ff74b85
Ayush <ayush26sehrawat@gmail.com>
2023-08-19 19:45:17 +0530

    Exception in java

---
+++ src/ClassesObjects/FileCheck.java
@@ -0,0 +1,17 @@
+package ClassesObjects;
+
+import java.io.File;
+public class FileCheck {
+    public static void main(String[] args) {
+        String filePath = "path/to/file/or/directory";
+        File file = new File(filePath);
+
+        if (file.exists()) {
+            if (file.isFile()) {
+                System.out.println("The path points to a file.");
+            } else if (file.isDirectory()) {
+                System.out.println("The path points to a directory.");
+            }
+        } else {
+            System.out.println("The file or directory does not exist.");  } } }
+
