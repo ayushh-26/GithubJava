25df6d9f6b46cab5a1002b2fcbbd5886df54c1d8
Ayush <ayush26sehrawat@gmail.com>
2023-08-19 18:52:46 +0530

    Added some extra files

--- src/DSA/Main.java
+++ src/DSA/Main.java
@@ -1,33 +1,17 @@
 package DSA;
 
+import java.util.Arrays;
+
 public class Main {
-
-        public static void main(String[] args) {
-            int[] arr = {1,9,3,8,5,6,7};
-            System.out.println("Elements at even index are: ");
-            even(arr);
-            System.out.println("Odd elements are: ");
-            odd(arr);
-            System.out.println("First element: " + arr[0]);
-            System.out.println("Last element: " + arr[arr.length-1]);
-
-            for (int i = arr.length-1; i>=0; i--) {
-                System.out.println(arr[i]);}
-        }
-        static int even(int arr[]){
-            for (int i = 0; i < arr.length; i+=2) {
-                System.out.println(arr[i]);
-            }
-            return 0;
-        }
-        static void odd(int[] arr){
-            for (int i = 0; i < arr.length; i++) {
-                if(i%2!=0){
-                    System.out.println(i);
-                }
-
-            }
-        }
-
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        swap(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
+    static void swap(int[] arr){
+        int temp = arr[0];
+        arr[0] = arr[arr.length-1];
+        arr[arr.length-1] = temp;
+    }
+}
